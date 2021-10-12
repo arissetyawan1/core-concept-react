79cb55f58596b98f15d224adefef43e752e7f9be
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-10-12 16:17:11 +0700

    Breakdown components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import "./App.css";
+// import compoents
+import Paper from "./component/paper";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <p>Nama Saya Aris Setyawan</p>
-      </header>
+      <Paper>
+        <section className="header">
+          <button className="header-btn black-color">Add</button>
+          <h1 className="header-title">Todo List</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+
+        <section className="add">
+          <form action="" className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn black-color">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React Js</span>
+          </div>
+        </section>
+      </Paper>
     </div>
   );
 }
---
+++ src/component/paper.jsx
@@ -0,0 +1,20 @@
+// Building reusable component
+import React from "react";
+import propTypes from "prop-types";
+
+// component Paper
+const Paper = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="frame">{children}</div>
+    </div>
+  );
+};
+
+Paper.propTypes = {
+  children: propTypes.oneOfType([
+    propTypes.arrayOf(propTypes.node),
+    propTypes.node,
+  ]),
+};
+export default Paper;
