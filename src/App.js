f1827f814528e9ec07c78d9379f7d03f21d89d28
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-10-12 12:25:37 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <p>Nama Saya Aris Setyawan</p>
       </header>
     </div>
   );
