58b2cca9d47576811c4d7bb27e50dee36b58cca8
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-10-13 11:28:53 +0700

    React Fundamental : Create state Using Hooks #1

--- src/component/todoform.jsx
+++ src/component/todoform.jsx
@@ -1,8 +1,30 @@
-const TodoForm = () => {
+import { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handlerFormInput = (e) => {
+    e.preventDefault();
+    if (!value) {
+      alert("Please insert a value input!!!");
+    } else {
+      // addTodo merupakan sebuah function props yang akan menerima nilai dari inputan yang akan dimasukan ke dalam list todo
+      addTodo(value);
+      alert(value);
+      setValue("");
+    }
+  };
+
+  console.log("value", value);
   return (
     <section className="add">
-      <form action="" className="add-form">
-        <input type="text" className="add-input" />
+      <form action="" className="add-form" onSubmit={handlerFormInput}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn black-color">Add</button>
       </form>
     </section>
